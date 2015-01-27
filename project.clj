4dbb11d3055269334f395414983f62ee30d474c2
johnsiraani <hi@johnsiraani.com>
2015-01-27 08:14:18 -0500

    Hello world program in clojure using luminus and ring

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject luminus "0.1.0-SNAPSHOT"
+:description "very basic ring application"
+:license {:name "Eclipse Public License"
+:url "http://www.eclipse.org/legal/epl-v10.html"}
+:dependencies [[org.clojure/clojure "1.5.1"]
+[ring/ring-core "1.2.1"]
+[ring/ring-jetty-adapter "1.2.1"]]
+:main luminus.core)
